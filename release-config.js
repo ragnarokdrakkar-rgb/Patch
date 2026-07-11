f0208d788a827fb38ecdc0121fce6b42e9b67dbb
Kemal Jazavac <kemal.jazavac@gmail.com>
2026-07-11 15:35:59 +0200

    Configure GitHub releases and updates

--- release-config.js
+++ release-config.js
@@ -1,5 +1,3 @@
-// Vpiši GitHub uporabniško ime oziroma organizacijo in ime JAVNEGA repozitorija.
-// Primer: owner: 'kemal123', repo: 'depo-injekcije-desktop'
 module.exports = {
   owner: 'ragnarokdrakkar-rgb',
   repo: 'Patch',
